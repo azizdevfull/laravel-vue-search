51d2e9d748937b27550c466ec7f3be408cbf244f
Azizbek Isroilov <aziz16110904@gmail.com>
2023-10-14 11:02:31 +0500

    completed simple search with laravel and vue js

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    public function index()
+    {
+        return Book::all();
+    }
+
+    public function search(Request $request)
+    {
+        $query = $request->input('q');
+        $results = Book::where('title', 'like', "%$query%")
+            ->orWhere('author', 'like', "%$query%")
+            ->get();
+
+        return $results;
+    }
 }
--- client/src/App.vue
+++ client/src/App.vue
@@ -1,47 +1,38 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div>
+        <input
+            v-model="searchQuery"
+            @input="search"
+            placeholder="Search books"
+        />
+        <ul>
+            <li v-for="book in books" :key="book.id">{{ book.title }}</li>
+        </ul>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import axios from "axios";
+export default {
+    data() {
+        return {
+            searchQuery: "",
+            books: [],
+        };
+    },
+    methods: {
+        search() {
+            axios
+                .get("http://localhost:8000/api/books/search", {
+                    params: { q: this.searchQuery },
+                })
+                .then((response) => {
+                    this.books = response.data;
+                });
+        },
+    },
+};
+</script>
+<style>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
