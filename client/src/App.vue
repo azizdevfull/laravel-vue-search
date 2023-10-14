f974d7e73e47c6dbfd34fee9123cd648bb742883
Azizbek Isroilov <aziz16110904@gmail.com>
2023-10-14 11:05:11 +0500

    fixed params to 3 words

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,38 +1,43 @@
 <template>
-    <div>
-        <input
-            v-model="searchQuery"
-            @input="search"
-            placeholder="Search books"
-        />
-        <ul>
-            <li v-for="book in books" :key="book.id">{{ book.title }}</li>
-        </ul>
-    </div>
+  <div>
+      <input
+          v-model="searchQuery"
+          @input="search"
+          placeholder="Search books"
+      />
+      <ul>
+          <li v-for="book in books" :key="book.id">{{ book.title }}</li>
+      </ul>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
-    data() {
-        return {
-            searchQuery: "",
-            books: [],
-        };
-    },
-    methods: {
-        search() {
-            axios
-                .get("http://localhost:8000/api/books/search", {
-                    params: { q: this.searchQuery },
-                })
-                .then((response) => {
-                    this.books = response.data;
-                });
-        },
-    },
+  data() {
+      return {
+          searchQuery: "",
+          books: [],
+      };
+  },
+  methods: {
+      search() {
+          if (this.searchQuery.length >= 3) {
+              axios
+                  .get("http://localhost:8000/api/books/search", {
+                      params: { q: this.searchQuery },
+                  })
+                  .then((response) => {
+                      this.books = response.data;
+                  });
+          } else {
+              // Clear the list if the search query is too short
+              this.books = [];
+          }
+      },
+  },
 };
 </script>
-<style>
 
+<style>
 </style>
